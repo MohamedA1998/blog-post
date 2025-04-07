a02e64aba494ef28bb8367308c902ee13b798c73
Mo7amed <mohamed.alsayed@cems-it.com>
2025-04-07 19:33:07 +0200

    handel register

---
+++ app/Http/Controllers/Api/Authentication/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\Authentication;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    public function store(Request $request)
+    {
+        $Validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($Validator->fails()){
+            return $this->apiResponse([], 422, $Validator->errors()->first());
+        }
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return $this->apiResponse([], 200, 'Regitser Completed Successfully');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Authentication\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('guest:api')->prefix('auth')->group(function(){
+    Route::post('register', [RegisterController::class, 'store']);
+});
 
 
